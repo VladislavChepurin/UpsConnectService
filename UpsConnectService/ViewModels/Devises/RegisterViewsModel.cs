eefca1db7f89890c82b45a03ba37d01a11d3a72b
Vladislav <vlad1988.1@gmail.com>
2023-01-23 18:42:32 +0300

    fix

--- UpsConnectService/ViewModels/Devises/RegisterViewsModel.cs
+++ UpsConnectService/ViewModels/Devises/RegisterViewsModel.cs
@@ -1,7 +1,17 @@
-﻿namespace UpsConnectService.ViewModels.Devises
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UpsConnectService.ViewModels.Devises
 {
     public class RegisterViewsModel
     {
+        [Required(ErrorMessage = "Поле Код обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Заводской номер", Prompt = "Введите заводской номер")]
+        public string? SerialNumber { get; set; }
 
+        [Required(ErrorMessage = "Поле Имя обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Код партии", Prompt = "Введите код партии")]
+        public string? ManufactureCode { get; set; }
     }
 }
---
+++ UpsConnectService/ViewModels/UserPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using UpsConnectService.ViewModels.Devises;
+using UpsConnectService.ViewModels.Users;
+
+namespace UpsConnectService.ViewModels
+{
+    public class UserPageViewModel
+    {
+        public UserViewModel? UserViewModel { get; set; }
+        public RegisterViewsModel? RegisterViewsModel { get; set; }
+    }
+}
